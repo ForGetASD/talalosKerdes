9c6ad201b3585050201dbd9b1cb63af245ab4a89
ForGetASD <vitayz60@gmail.com>
2022-06-06 21:01:42 +0200

    Változók és Konstruktor létrehozása

--- src/model/Lada.java
+++ src/model/Lada.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Lada {
+    private String anyag;
+    private String szoveg;
+    private boolean nalaVan;
+
     
+    public Lada(String anyag, String szoveg){
+        this(anyag, szoveg, false);
+    }
+    
+    public Lada(String anyag, String szoveg, boolean nalaVan){
+        this.anyag = anyag;
+        this.szoveg = szoveg;
+        this.nalaVan = nalaVan;
+    }
 }
